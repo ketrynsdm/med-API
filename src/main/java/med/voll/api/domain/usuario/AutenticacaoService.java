a2c3dd3de9ead0c6fb0850a2a0de10c66f3e72f7
ketryn <ketryn29@gmail.com>
2023-01-23 17:23:45 -0300

    Authentication JWT

---
+++ src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -0,0 +1,15 @@
+package med.voll.api.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AutenticacaoService implements UserDetailsService {
+    @Autowired
+    private UsuarioRespository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
